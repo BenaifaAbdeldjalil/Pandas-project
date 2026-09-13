8624e7ff6bc066012932d8c871cee6546af73f27
BENAIFA Abdeldjalil <jalil3atrchi@gmail.com>
2026-09-13 18:10:04 +0200

    add modifications .\download_communes.py  2026-09-13 18:10:04

--- test_api.py
+++ test_api.py
@@ -1,12 +1,27 @@
 import requests
+import json
+
+
+
+
+
+
+
+
 url = "https://geo.api.gouv.fr/communes?fields=nom,code,codeDepartement,population"
-response = requests.get(url,timeout=30)
+try:
+    response = requests.get(url, timeout=30)
+    response.raise_for_status() 
+        # Vérifier que la requête a réussi
+    print(response.raise_for_status() )
+    if response.raise_for_status() is None:
+        data = response.json()
+
+            # Écrire directement la réponse JSON dans un fichier
+        with open("data/raw/communes_raw.json", "w", encoding="utf-8") as f:
+            json.dump(response.json(), f, indent=4, ensure_ascii=False)
+                
+except:
+        print("error")
 
 
-
-statut = response.status_code
-raise_statut = response.raise_for_status()
-print(statut,raise_statut)
-data = response.json()
-
-print(data[:5])
