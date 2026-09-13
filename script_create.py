d56b5d88f97a6b0c1c691ec2bb0bca1c670e4de7
BENAIFA Abdeldjalil <jalil3atrchi@gmail.com>
2026-09-13 19:11:52 +0200

    add modifications .\script_create.py  2026-09-13 19:11:52

--- script_create.py
+++ script_create.py
@@ -1,12 +1,11 @@
 import os
 from pathlib import Path
 
-folders = ["data","scripts","src","data/raw/","docs","data/processed/","data/final/"]
-
-base = Path(".")
-
+folders = ["data","scripts","src","data/raw/","docs","data/processed/"
+           ,"data/final/","data/final/by_departement/"]
 
 base = Path(".")  # dossier courant
+
 # 1) folders creation
 for f in folders:
     path = base / f
@@ -18,16 +17,24 @@ for f in folders:
 
 print("\nContenu actuel :", os.listdir())
 
+
+# 2) files creation
 files = ["data/processed/test.txt","data/raw/test.txt",
          "scripts/01_download_data.py","scripts/02_clean_data.py",
          "scripts/03_split_by_departement.py","scripts/04_stats_departements.py",
-         "src/clean.py","src/download.py",
-         "src/split_dept.py"]
-# 2) files creation
+         "src/clean.py","src/download.py","docs/GUIDE.md","src/__init__.py","src/stats.py",
+         "src/split_dept.py","requirements.txt",".gitignore","README.md"]
+
 for fi in files:
     file = base/fi
     if file.exists():
         print(f"⏭️  Le fichier { file } existe déjà")
     else:
-        file.write_text("Hello\n", encoding="utf-8")
+        file.write_text("# -*- coding: utf-8 -*- \n", encoding="utf-8")
         print(f"✅ Fichier créé : { file.resolve() }")
+
+for f in ["data/processed/test.txt", "data/raw/test.txt"]:
+    p = Path(f)
+    if p.exists():
+        p.unlink()
+        print(f"🗑️  Supprimé : {p}")
