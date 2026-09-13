de57f3874ed5ba25a9297b5d6f262aa5cbbd6b3a
BENAIFA Abdeldjalil <jalil3atrchi@gmail.com>
2026-09-13 17:05:57 +0200

    add modifications .\script_create.py  2026-09-13 17:05:56

--- script_create.py
+++ script_create.py
@@ -1,13 +1,26 @@
-import os,sys
+import os
 from pathlib import Path
 
-folder = ["data","src","data/raw/","data/processed/","data/final/"]
+folders = ["data","src","data/raw/","data/processed/","data/final/"]
 
-path1=Path(".")
+base = Path(".")
 
-for i in folder:
-    print(path1.exists(),i)
-    if path1:
-        pass
-    else : 
-        os.chdir(i)
+
+base = Path(".")  # dossier courant
+
+for f in folders:
+    path = base / f
+    if not path.exists():
+        path.mkdir(parents=True, exist_ok=True)
+        print(f"✅ Créé : {path}")
+    else:
+        print(f"⏭️  Existe déjà : {path}")
+
+print("\nContenu actuel :", os.listdir())
+
+
+# 2) Créer test.txt dans data/processed
+fichier = base / "data/processed/test.txt"
+fichier.write_text("Hello\n", encoding="utf-8")
+
+print(f"✅ Fichier créé : {fichier.resolve()}")
