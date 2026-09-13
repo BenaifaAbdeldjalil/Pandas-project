13c0654f47db16c3f84696f08cc31114a12e01c0
BENAIFA Abdeldjalil <jalil3atrchi@gmail.com>
2026-09-13 18:12:18 +0200

    add modifications .\README.md  2026-09-13 18:12:17

--- test_api.py
+++ test_api.py
@@ -1,19 +1,11 @@
 import requests
 import json
 
-
-
-
-
-
-
-
 url = "https://geo.api.gouv.fr/communes?fields=nom,code,codeDepartement,population"
 try:
     response = requests.get(url, timeout=30)
     response.raise_for_status() 
         # Vérifier que la requête a réussi
-    print(response.raise_for_status() )
     if response.raise_for_status() is None:
         data = response.json()
 
